73fd3efa6e1c5b7340584447e0b04ff58303921b
PRACHI0 <prachityagi447@gmail.com>
2017-08-26 03:06:31 +0530

    second commit

---
+++ section2solution/exceptionhandling.py
@@ -0,0 +1,220 @@
+
+try:
+    import beautifulsoup4  
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+
+try:
+    import boto
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+
+
+try:
+    import bz2file
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+
+try:
+    import certifi 
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+
+try:
+    import chardet
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+
+try:
+    import gensim
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+
+try:
+    import html5lib
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+
+try:
+    import idna
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+try:
+    import nltk
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+
+try:
+    import numpy
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+try:
+    import pexpect
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+try:
+    import pip
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+
+try:
+    import ptyprocess
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+    
+try:
+    import pyxdg
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+
+
+try:
+    import reportlab
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+try:
+    import requests
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+try:
+    import scipy
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+try:
+    import setuptools
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+
+try:
+    import six
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+try:
+    import smartopen
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+try:
+    import textblob
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+try:
+    import twitter
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+try:
+    import urllib3
+    print('\nModule was installed')
+except ImportError:
+    print('\nThere was no such module installed')
+
+    
+
+
+
+
+    
+
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    
+
